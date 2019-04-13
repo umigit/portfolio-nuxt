88c95592a995895d00c1da9193c22bb8f37f68f6
umigit <hamu56Kiki>
2019-04-13 19:24:32 +0900

    add header script

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,10 +5,20 @@ module.exports = {
   */
   head: {
     title: 'portfolio-nuxt',
+    script: [
+      { async src="https://www.googletagmanager.com/gtag/js?id=UA-138222932-1" },
+      {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){ dataLayer.push(arguments); };
+        gtag('js', new Date());
+
+        gtag('config', 'UA-138222932-1');
+      },
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: "Umihiko's portfolio" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
